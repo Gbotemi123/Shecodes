36783d3463fbd3ca4a8c905cc3e3a939f1335bec
Kayode Oluwatosin <160548748+Gbotemi123@users.noreply.github.com>
2024-02-26 00:24:06 +0100

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+name = input("what is your name")
+age = input("how old are you")
+location = input("where is your location")
+print("Hello, {name}")
+print("you are based in {location}") 
+print("you are currently {age} years old")
+
+
+
+
